554b8bf02ff75ac926349aa66f3fca8817ee023f
suya santos <suyasantos22@gmail.com>
2026-02-27 19:02:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7459781731-b3940",
+  "appId": "1:542510388129:web:bbfeddcf106d28c28df1e0",
+  "apiKey": "AIzaSyChbHL5HUvlV9WbqLFNJX2muidhaVugRV0",
+  "authDomain": "studio-7459781731-b3940.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "542510388129"
+};
